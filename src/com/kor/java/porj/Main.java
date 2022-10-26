81f2a556a0f9d1bd40dafebe4f6e38b5a649da19
rnfmzk23 <rnfmzk23@gmail.com>
2022-10-26 20:16:29 +0900

    스캐너 입력받기

---
+++ src/com/kor/java/porj/Main.java
@@ -0,0 +1,22 @@
+package com.kor.java.porj;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.printf("명령어를 입력해주세요 : ");
+		String command = sc.next();
+		System.out.printf("입력된 명령어 : %s \n", command);
+		
+		System.out.printf("명령어를 입력해주세요 : ");
+		int num = sc.nextInt();
+		System.out.printf("입력된 숫자 : %d \n", num);
+		
+		sc.close();
+		
+		System.out.println("== 프로그램 끝 ==");
+	}
+}
